b9a0326f50a37af399379cf61c87d7d40f519839
cyberdanes <sandeep.cyberdanes@gmail.com>
2014-07-27 22:14:30 +0530

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,13 @@ router.get('/homepage', function(req, res) {
 });
 
 /* GET New Video page. */
-router.get('/newvideo', function(req, res) {
-    res.render('newvideo', { title: 'Add New Video' });
+router.get('/newstatus', function(req, res) {
+    res.render('newstatus', { title: 'Add New Status' });
 });
 
 /* GET New Video page. */
-router.get('/searchvideopage', function(req, res) {
-    res.render('searchvideopage', { title: 'Search Videos' });
+router.get('/searchstatus', function(req, res) {
+    res.render('searchstatus', { title: 'Search Status' });
 });
 
 /* GET novideospage. */
@@ -51,201 +51,99 @@ router.get('/videolist', function(req, res) {
  });
 });
 
-/* POST to search videos. */
-router.post('/searchvideo', function(req, res) {
-    var db = req.db;
-    var matchingVideos = [];
-    var tags = req.body.keywords;
-    var arr = tags.split(" ");
-    
-    // app.js
-    db.videos.find({}, function(err, videos) {
-      if( err || !videos) 
-      res.redirect("novideospage");
-      else videos.forEach( function(video) {
-      var videoTags = video.tags;
-      var videoTagArr = videoTags.split(" ");
-      videoTagArr.forEach(function(value1) {
-      	arr.forEach(function(value2) {
-      		if(value1 === value2)
-      		match = true;
-      	});
-      	if(match === true)
-      	matchingVideos.push(video);
-      	match = false;
-      });
-  } );
-    });
-
- //   var collection = db.get('videocollection');
-//    collection.find({},{},function(e,docs){
-//        res.render('videolist', {
-//            "videolist" : docs
-//        });
-//    });
-});
-
 /* POST to add video service*/
-router.post('/addvideo', function(req, res) {
+router.post('/addstatus', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
     var mongodb = require('mongodb');
 
     // Get our form values. These rely on the "name" attributes
-    var videoname = req.body.videoname;
-    var videoid = req.body.videoid;
-    var videotags = req.body.videotags;
-    var videopath = req.body.videopath;
-
-var videoData = [
+    var username = req.body.username;
+    var yesterday = req.body.yesterday;
+    var today = req.body.today;
+    var impediments = req.body.impediments;
+    var rawstatusdate = new Date();
+    console.log('status date' , rawstatusdate);
+    var statusmonth = rawstatusdate.getMonth() + 1;
+    var statusdate = rawstatusdate.getDate() + "/" + statusmonth + "/" + rawstatusdate.getFullYear();
+    console.log('formatted date', statusdate)
+var statusData = [
   {
-    videoName: videoname,
-    videoId: videoid,
-    videoTags: videotags,
-    videoPath: videopath
+    "userName": username,
+    "statusDate": statusdate,
+    "yesterday": yesterday,
+    "today": today,
+    "impediments": impediments
   }
 ];
 
-var uri = 'mongodb://motu:motu1234@ds045679.mongolab.com:45679/cyberdanes';
+var mongo = require('mongodb');
+var Server = mongo.Server;
+var Db = mongo.Db;
+var result;
 
-mongodb.MongoClient.connect(uri, function(err, db) {
-  
-  if(err)  res.send("There was a problem adding the information to the database.");;
-  
-       var videos = db.collection('videos');
 
-   // Note that the insert method can take either an array or a dict.
-
- videos.insert(videoData, function(err, result) {
-    if(err) throw err;
-    else {
-          res.location("videolist");
-          //And forward to success page
-            res.redirect("videolist");
-    }
+var server = new Server('ds053459.mongolab.com', 53459, {auto_reconnect : true});
+console.log('got server');
+var db = new Db('statusapp', server);
+db.open(function(err, client) {
+    client.authenticate('statusapp', 'statusapp', function(err, success) {
+        console.log('connected to db');
+        var statusCollection = db.collection("dailyUpdateCollection");
+        console.log('got db collection');
+        statusCollection.insert(statusData);
+        if(success){
+          console.log('inserted data');
+          result = 'success';
+        }else{
+          console.log('error occurred inserting data');
+          result = 'error';
+        }
+        
+        db.close();
+    });
+});
+res.redirect("homepage");
 });
 
+    /* POST to search status. */
+    router.post('/searchstatus', function(req, res) {
+        var username = req.body.username;
+        var date = req.body.date;
+        console.log(username);
+        console.log(date);
+        var mongo = require('mongodb');
+var Server = mongo.Server;
+var Db = mongo.Db;
+var result;
 
-  
-});
-
-    // Submit to the DB
-//    collection.insert({
-//        "videoname" : videoName,
-//        "videoid" : videoId,
-//        "videotags" : videoTags,
-//	"videopath" : videoPath
-//    }, function (err, doc) {
-//        if (err) {
-//            // If it failed, return error
-//            res.send("There was a problem adding the information to the database.");
-//        }
-//        else {
-//            // If it worked, set the header so the address bar doesn't still say /adduser
-//            res.location("homepage");
-//            // And forward to success page
-//            res.redirect("homepage");
-//        }
-//    });
-});
-
-    /* POST to search videos. */
-    router.get('/searchvideo', function(req, res) {
-        var url = require('url');
-        var url_parts = url.parse(req.url, true);
-        var tags = url_parts.query;
-        var db = req.db;
-        var videos = db.get('videocollection');
-        var matchingVideos = [];
-        var arr = tags.split(" ");
-        var match = false;
-        console.log("in method");
-        var flag1 = 1;
-        var flag2 = 1;
-        var flag3 = 1;
-        var videoPushCount = 0;
-        // app.js
-        videos.find({}, function(err, videos) {
-          if( err || !videos) 
-          res.redirect("novideospage");
-          else videos.forEach( function(video) {
-            console.log('Flag One for Video', flag1);
-          var videoTags = video.videotags;
-          var videoPushed = false;
-          arr.forEach(function(value1) {
-            console.log('Flag Two for keyword' ,flag2);
-            videoTags.forEach(function(value2) {
-            console.log('Flag Three for Video tag' ,flag3);
-            console.log('Value from video', value1);
-            console.log('Value from keyword', value2);
-              if(value1.toString().toUpperCase() === value2.toString().toUpperCase()){
-              match = true;
-              console.log('match');
-            }
-            flag3 = flag3 +1;
-            });
-            flag3 = 1;
-            flag2 = flag2 + 1;
-          });
-          flag2 = 1;
-          if(match === true && videoPushed === false){
-            matchingVideos.push(video);
-            videoPushed = true;
-            videoPushCount = videoPushCount +1;
-            console.log('video pushed', videoPushCount);
-            console.log(video);
-            console.log(video.videopath);
-            console.log(matchingVideos);
-          }
-          match = false;
-      });
-    console.log('final matching video paths' , matchingVideos);
-        //res.json(matchingVideos);
-           res.writeHead(200, {"Content-Type":"text/json",
+var server = new Server('ds053459.mongolab.com', 53459, {auto_reconnect : true});
+console.log('got server');
+var db = new Db('statusapp', server);
+console.log('got db');
+db.open(function(err, client) {
+    client.authenticate('statusapp', 'statusapp', function(err, success) {
+        console.log('connected to db');
+        var statusCollection = db.collection("dailyUpdateCollection");
+        console.log('got db collection');
+       statusCollection.find({"userName":username, "statusDate":date},{}, function(err, statuses) {
+      if( err || !statuses) 
+      res.redirect("novideospage");
+      else {
+        res.writeHead(200, {"Content-Type":"text/json",
                 "Access-Control-Allow-Origin": "*"});
-   res.write(JSON.stringify(matchingVideos));
-   res.end();
-        });
+        statuses.each(function(t, status) { 
+        console.log('status', status);
+        console.log('stringified', JSON.stringify(status));
+          console.log('writing to response');
+          res.write(JSON.stringify(status));
+          res.end();
+      });
+      };
+  });
     });
-
-    /* POST to add video service*/
-    router.post('/addvideo', function(req, res) {
-
-        // Set our internal DB variable
-        var db = req.db;
-       
-        // Get our form values. These rely on the "name" attributes
-        var videotitle = req.body.videotitle;
-        var videodescription = req.body.videodescription;
-        var videopath = req.body.videopath;
-        var videoposter = req.body.videoposter;
-        var videocategory = req.body.videocategory;
-        var videotags = req.body.videotags;
-        var tagarr = videotags.split(" ");
-    
-    var collection = db.get('videocollection');
-      
-      // Submit to the DB
-    collection.insert({
-        "videotitle" : videotitle,
-        "videodescription" : videodescription,
-        "videopath" : videopath,
-        "videoposter" : videoposter,
-        "videocategory" : videocategory,
-        "videotags" : tagarr
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /addvideo
-            res.location("homepage");
-            // And forward to success page
-            res.redirect("homepage");
-        }
-    });
-    });
+});
+});
 
 module.exports = router;
